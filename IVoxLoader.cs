05581785fad2d9e7102463738e0078501a339dd7
barraudf <barraudf@gmail.com>
2017-12-05 13:28:13 +0100

    First implementation

---
+++ IVoxLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpVoxReader
+{
+    public interface IVoxLoader
+    {
+        void LoadModel(Int32 sizeX, Int32 sizeY, Int32 sizeZ, byte[,,] data);
+        void LoadPalette(UInt32[] palette);
+        void SetModelCount(Int32 count);
+        void SetMaterialOld(Int32 paletteId, Chunks.MaterialOld.MaterialTypes type, float weight, Chunks.MaterialOld.PropertyBits property, float normalized);
+    }
+}
